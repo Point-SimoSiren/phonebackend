5a8f02f4943273af3c18f3efe2a00473e91b15ce
Simo Siren <simo.siren@stud.pointcollege.fi>
2019-10-05 10:02:11 +0300

    test

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
 
 let persons = [
     {
@@ -14,8 +17,49 @@ let persons = [
     }
 ]
 
-app.get('/notes', (req, res) => {
+//GET ALL
+app.get('/api/persons', (req, res) => {
     res.json(persons)
+})
+
+app.get('/api/info', (req, res) => {
+    res.send('<h2>info</h2>')
+})
+
+//GET 1 PERSON
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+//GENERATE ADD FOR ADDING NEW PERSON
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(p => p.id))
+        : 0
+    return maxId + 5
+}
+
+//ADD NEW PERSON
+app.post('/api/notes', (request, response) => {
+    const body = request.body
+
+    if (!body.name || !body.phone) {
+        return response.status(400).json({
+            error: 'name or number or both are missing'
+        })
+    }
+    const newPerson = {
+        name: body.name,
+        phone: body.phone,
+        id: generateId()
+    }
+    persons = persons.concat(newPerson)
+    response.json(newPerson)
 })
 
 const port = 3001
